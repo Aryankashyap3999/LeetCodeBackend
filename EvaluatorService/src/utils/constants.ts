81b5c3d4d83d6bc799d37ef1488fdce534e00adc
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-12-17 20:18:59 +0530

    Added image pulling utility

--- EvaluatorService/src/utils/constants.ts
+++ EvaluatorService/src/utils/constants.ts
@@ -1,1 +1,5 @@
 export const SUBMISSION_QUEUE_NAME: string = "submission";
+
+export const PYTHON_DOCKER_IMAGE: string = "python:3.9-slim";
+
+export const CPP_DOCKER_IMAGE: string = "gcc:latest";
---
+++ EvaluatorService/src/utils/containers/pullImage.util.ts
@@ -0,0 +1,39 @@
+import dockerode from 'dockerode';
+import { CPP_DOCKER_IMAGE, PYTHON_DOCKER_IMAGE } from '../constants';
+import logger from '../../config/logger.config';
+
+
+export async function pullImage(image: string) {
+    const docker = new dockerode();
+
+    return new Promise((res, rej) => {
+        docker.pull(image, (err: Error, stream: NodeJS.ReadableStream) => {
+            if(err) return err;
+
+            docker.modem.followProgress(
+                stream, 
+                function onFinished(finalErr, output) {
+                    if(finalErr) return rej(finalErr);
+                    res(output);
+                },
+                function onProgress(event) {
+                    console.log(event.status);
+                }
+            )
+        })
+    })
+}
+
+export async function pullAllImages() {
+    const images = [PYTHON_DOCKER_IMAGE, CPP_DOCKER_IMAGE];
+
+    const pullPromises = images.map(image => pullImage(image));
+
+    try {
+        await Promise.all(pullPromises);
+        logger.info("All Docker images pulled successfully");
+    } catch (error) {
+        logger.error("Error pulling Docker images:", error);
+
+    }
+}
